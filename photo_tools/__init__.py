0c714a86a76be99c103d722a1f86a892a82d24f5
Andoni Sooklaris <andonis@apple.com>
2021-10-04 15:58:05 -0500

    Integrated versioneer

---
+++ photo_tools/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Photo Tools."""
+
+__author__ = """Andoni Sooklaris"""
+__email__ = 'andoni.sooklaris@gmail.com'
+__version__ = '0.1.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
